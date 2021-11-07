3a8fd016689ede19863261d70208a09e62c4da5a
emreturgutce <emreturgutcse@hotmail.com>
2021-11-07 14:30:41 +0300

    using select with channels

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,26 @@
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.selects.select
 
 fun main() = runBlocking {
-    val channel = Channel<String>()
+    val message = producer()
+    val message2 = producer2()
 
-    launch(coroutineContext) { sendString(channel, "foo", 200L) }
-    launch(coroutineContext) { sendString(channel, "bar", 500L) }
-
-    repeat(6) {
-        println(channel.receive())
-    }
-
-    coroutineContext.cancelChildren()
+    repeat(15) { println(selector(message, message2)) }
 }
 
-suspend fun sendString(channel: Channel<String>, s: String, interval: Long) {
-    while (true) {
-        delay(interval)
-        channel.send(s)
+fun producer() = GlobalScope.produce {
+    send("From Producer 1")
+}
+
+fun producer2() = GlobalScope.produce {
+    send("From Producer 2")
+}
+
+suspend fun selector(message: ReceiveChannel<String>, message2: ReceiveChannel<String>): String {
+    return select {
+        message.onReceiveCatching { it.getOrNull() ?: "Channel 1 is closed" }
+        message2.onReceiveCatching { it.getOrNull() ?: "Channel 2 is closed" }
     }
 }
