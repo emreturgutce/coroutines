d93fe7ce7d301a317bfa4222aa585c0e6acfb37a
Emre Turgut <emre.turgut@trendyol.com>
2021-11-14 16:01:52 +0300

    cancel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,21 @@
 package com.emreturgutce
 
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) = runBlocking {
-
     val job = launch {
-        delay(1000)
-        println("World")
+        repeat(1000) {
+            delay(10)
+            print(".")
+        }
     }
 
-    print("Hello")
+    delay(250)
 
-    job.join()
+    job.cancelAndJoin()
+
+    println("Done")
 }
