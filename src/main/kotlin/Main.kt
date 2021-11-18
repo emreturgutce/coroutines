3fe24548c6d6df08fe382403c791dc7189f01d81
emreturgutce <emreturgutcse@hotmail.com>
2021-11-18 16:56:13 +0300

    cooperative cancellation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,30 +3,32 @@ package com.emreturgutce
 import kotlinx.coroutines.*
 
 fun main(args: Array<String>) = runBlocking {
-    val launchParent = Job()
-    val scope = CoroutineScope(Job())
+    var child1: Job? = null
 
-    val job = scope.launch(launchParent) {
-        val j1 = coroutineContext[Job]
-        val j2 = launch { delay(500) }
-        println("job passed to the scope.launch as the new context: $launchParent")
-        displayChildren(0, launchParent)
-        println("job returned from scope.launch as the new job: $j1")
-        displayChildren(0, j1!!)
-        println("job returned from child launch (j2): $j2")
-        displayChildren(0, j2)
-        j2.join()
-    }
+    coroutineScope {
+        val job = GlobalScope.launch {
+            child1 = launch {
+                repeat(1000) {
+                    Thread.sleep(1000)
+                    print("1")
+                    yield()
+                }
+            }
 
-    job.join()
-}
-
-fun displayChildren(depth: Int = 0, job: Job) {
-    job.children.forEach {
-        for (i in 0..depth) {
-            print("\t")
+            repeat(1000) {
+                delay(1000)
+                println("0")
+            }
         }
-        println("child: $it")
-        displayChildren(depth + 1, it)
+
+        delay(4000)
+        child1?.cancelAndJoin()
+        println()
+        println("job is cancelled: ${job.isCancelled}")
+        println("job is active: ${job.isActive}")
+
+        job.join()
     }
+
+    println("coroutineScope finished")
 }
