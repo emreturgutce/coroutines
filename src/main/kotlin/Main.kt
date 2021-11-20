436fe14554e00c5f5c43a82a199d2cb72c1a7cf0
emreturgutce <emreturgutcse@hotmail.com>
2021-11-20 17:34:03 +0300

    simple select

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,39 @@
 package com.emreturgutce
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.SendChannel
-import java.util.*
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.selects.select
 
-const val TOTAL_WORK_ITEMS = 200
-const val TOTAL_AGENTS = 10
+fun CoroutineScope.producer1() = produce<String>() {
+    var count = 100
 
-data class Work(
-    val x: Long,
-    val y: Long,
-    var total: Long = 0L
-)
-
-suspend fun agent(input: ReceiveChannel<Work>, output: SendChannel<Work>) {
-    for (work in input) {
-        work.total = work.x * work.y
-        delay(work.total)
-        output.send(work)
+    while (true) {
+        send("from producer 1: ${count++}")
     }
 }
 
-suspend fun producer(output: SendChannel<Work>) {
-    repeat(TOTAL_WORK_ITEMS) {
-        output.send(Work((0L..100).random(), (0L..100).random()))
+fun CoroutineScope.producer2() = produce<String>() {
+    var count = 200
+
+    while (true) {
+        send("from producer 2: ${count++}")
     }
-}
-
-suspend fun consumer(input: ReceiveChannel<Work>) {
-    for (work in input) {
-        println("${work.x}*${work.y} = ${work.total}")
-    }
-}
-
-suspend fun CoroutineScope.run() {
-    val producerChannel = Channel<Work>()
-    val consumerChannel = Channel<Work>()
-
-    repeat(TOTAL_AGENTS) {
-        launch {
-            agent(producerChannel, consumerChannel)
-        }
-    }
-
-    launch { producer(producerChannel) }
-    launch { consumer(consumerChannel) }
 }
 
 fun main(args: Array<String>) = runBlocking {
-    run()
+
+    val p1 = producer1()
+    val p2 = producer2()
+
+    repeat(15) {
+        select<Unit> {
+            p1.onReceive(::println)
+            p2.onReceive(::println)
+        }
+    }
+
+    println("Finished")
+
+    p1.cancel()
+    p2.cancel()
 }
-
-private object RandomRangeSingleton : Random()
-
-fun ClosedRange<Long>.random() = (RandomRangeSingleton.nextInt((endInclusive.toInt() + 1) - start.toInt()) + start)
