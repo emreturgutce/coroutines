58268a0164bc450d8190b3d51411b0d59b3b1ecb
emreturgutce <emreturgutcse@hotmail.com>
2021-11-20 12:19:50 +0300

    pipelining

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,49 @@ import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.produce
 
-suspend fun CoroutineScope.produceSquares(): ReceiveChannel<Int> = produce {
-    for (x in 1..5) {
-        val num = x * x
-        println("Sending $num")
-        send(num)
+suspend fun produceNumbers(coroutineScope: CoroutineScope): ReceiveChannel<Int> =
+    coroutineScope.produce {
+        var x = 1
+
+        while (true) {
+            send(x++)
+            delay(500)
+        }
+
     }
 
+suspend fun createSquares(coroutineScope: CoroutineScope, numbersProducer: ReceiveChannel<Int>): ReceiveChannel<Int> =
+    coroutineScope.produce {
+        for (value in numbersProducer) send(value * value)
+    }
+
+suspend fun consume(coroutineScope: CoroutineScope, producer: ReceiveChannel<Int>) = coroutineScope.launch {
+    producer.consumeEach { println("Received $it") }
 }
 
-fun main(args: Array<String>) = runBlocking<Unit> {
-    val channel = produceSquares()
+suspend fun <T, R> ReceiveChannel<T>.map(
+    coroutineScope: CoroutineScope,
+    transform: (T) -> R
+): ReceiveChannel<R> {
+    val receiveChannel = this
 
-    channel.consumeEach { println("Received $it") }
+    return coroutineScope.produce {
+        try {
+            for (value in receiveChannel) send(transform(value))
+        } catch (ex: CancellationException) {
+            receiveChannel.cancel()
+        }
+    }
+}
+
+fun main(args: Array<String>) = runBlocking {
+    val producer = produceNumbers(this).map(this) {
+        it * it * it
+    }
+
+    consume(this, producer)
+
+    delay(3000)
+
+    producer.cancel()
 }
