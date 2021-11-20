1917e61c151b29a748cdebf75b4425b6ca2a9b33
emreturgutce <emreturgutcse@hotmail.com>
2021-11-20 17:23:34 +0300

    demo load balancer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,31 +2,57 @@ package com.emreturgutce
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import java.util.*
 
-fun main(args: Array<String>) = runBlocking {
-    val channel = Channel<Int>()
+const val TOTAL_WORK_ITEMS = 200
+const val TOTAL_AGENTS = 10
 
-    launch { receiveData("First", channel) }
-    launch { receiveData("Second", channel) }
-    launch { receiveData("Third", channel) }
-    launch { receiveData("Fourth", channel) }
+data class Work(
+    val x: Long,
+    val y: Long,
+    var total: Long = 0L
+)
 
-    delay(100)
-
-    channel.send(1)
-    channel.send(2)
-    channel.send(3)
-    channel.send(4)
-    channel.send(5)
-    channel.send(6)
-
-    delay(1000)
-
-    coroutineContext.cancelChildren()
-}
-
-suspend fun receiveData(name: String, channel: Channel<Int>) {
-    for (value in channel) {
-        println("$name: $value")
+suspend fun agent(input: ReceiveChannel<Work>, output: SendChannel<Work>) {
+    for (work in input) {
+        work.total = work.x * work.y
+        delay(work.total)
+        output.send(work)
     }
 }
+
+suspend fun producer(output: SendChannel<Work>) {
+    repeat(TOTAL_WORK_ITEMS) {
+        output.send(Work((0L..100).random(), (0L..100).random()))
+    }
+}
+
+suspend fun consumer(input: ReceiveChannel<Work>) {
+    for (work in input) {
+        println("${work.x}*${work.y} = ${work.total}")
+    }
+}
+
+suspend fun CoroutineScope.run() {
+    val producerChannel = Channel<Work>()
+    val consumerChannel = Channel<Work>()
+
+    repeat(TOTAL_AGENTS) {
+        launch {
+            agent(producerChannel, consumerChannel)
+        }
+    }
+
+    launch { producer(producerChannel) }
+    launch { consumer(consumerChannel) }
+}
+
+fun main(args: Array<String>) = runBlocking {
+    run()
+}
+
+private object RandomRangeSingleton : Random()
+
+fun ClosedRange<Long>.random() = (RandomRangeSingleton.nextInt((endInclusive.toInt() + 1) - start.toInt()) + start)
