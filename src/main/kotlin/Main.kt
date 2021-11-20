1e7634a6d1cffff1bc09875b8d4166b7c2a94f7a
emreturgutce <emreturgutcse@hotmail.com>
2021-11-20 15:35:57 +0300

    fairness

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,29 +4,29 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 
 fun main(args: Array<String>) = runBlocking {
-    val channel = Channel<Int>(4)
+    val channel = Channel<Int>()
 
-    val producer = launch {
-        repeat(4) {
-            println("Sending $it")
-            channel.send(it)
-        }
+    launch { receiveData("First", channel) }
+    launch { receiveData("Second", channel) }
+    launch { receiveData("Third", channel) }
+    launch { receiveData("Fourth", channel) }
+
+    delay(100)
+
+    channel.send(1)
+    channel.send(2)
+    channel.send(3)
+    channel.send(4)
+    channel.send(5)
+    channel.send(6)
+
+    delay(1000)
+
+    coroutineContext.cancelChildren()
+}
+
+suspend fun receiveData(name: String, channel: Channel<Int>) {
+    for (value in channel) {
+        println("$name: $value")
     }
-
-    val consumer = launch {
-        println("Consumer starting")
-        delay(1000)
-        println("Consumer started")
-
-        for (value in channel) {
-            println(" -- received $value")
-        }
-    }
-
-    delay(500)
-
-    println("Cancelling producer")
-
-    producer.cancel()
-    consumer.join()
 }
