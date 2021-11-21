fbea7046cb8646502e85a91a55115dbefcd7e347
emreturgutce <emreturgutcse@hotmail.com>
2021-11-21 18:01:21 +0300

    catching

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,34 @@
 package com.emreturgutce
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.take
 
-fun main(args: Array<String>) = runBlocking<Unit> {
+fun generateInts() = flow {
+    var value = 0
 
-    val counter = Counter()
-
-    launch {
-        while (true) {
-            counter.increment()
-            delay(100)
-        }
-    }
-
-    delay(500)
-
-    launch {
-        println("Collector (A) before ${counter.counter.value}")
-
-        counter.counter.collect {
-            delay(1000)
-            println("Collector (A) $it")
-        }
+    while (true) {
+//        check(value < 2) { "emitter" }
+        delay(100)
+        emit(value++)
     }
 }
 
-class Counter {
-    private val _counter = MutableStateFlow(0)
+fun main(args: Array<String>) = runBlocking<Unit> {
 
-    val counter = _counter.asStateFlow()
-
-    fun increment() {
-        _counter.update { it + 1 }
+    launch {
+        try {
+            generateInts()
+                .take(5)
+                .collect {
+                    check(it < 3) { "collector" }
+                    println("Collected $it")
+                }
+        } catch (t: Throwable) {
+            println(t.message)
+        } finally {
+            println("Completed")
+        }
     }
 }
