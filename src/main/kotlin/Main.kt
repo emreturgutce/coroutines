36140d0cfa1dbf288100456c35ba0168773c5e13
emreturgutce <emreturgutcse@hotmail.com>
2021-11-21 15:53:17 +0300

    combining flows

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package com.emreturgutce
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import java.util.*
 import kotlin.system.measureTimeMillis
 
 fun generateInts() = flow {
@@ -14,15 +15,35 @@ fun generateInts() = flow {
     }
 }
 
+fun generateRandomInts() = flow {
+    var random = Random(100)
+
+    while (true) {
+        val value = random.nextInt(20)
+        delay(1000)
+        println("emit random: $value")
+        emit(value)
+    }
+}
+
 fun main(args: Array<String>) = runBlocking<Unit> {
+
+    var startTime = System.currentTimeMillis()
+
+    val job = launch {
+        generateInts()
+            .take(5)
+            .zip(generateRandomInts()) { a, b -> "$a * $b" }
+            .collect { println("zipped at ${System.currentTimeMillis() - startTime}") }
+    }
+
+    job.join()
+
+    startTime = System.currentTimeMillis()
 
     launch {
         generateInts()
-            .take(5)
-            .collectLatest {
-                println("Collecting $it")
-                delay(300)
-                println("Collected $it")
-            }
+            .combine(generateRandomInts()) { a, b -> "$a * $b" }
+            .collect { println("combined $it at ${System.currentTimeMillis() - startTime}") }
     }
 }
